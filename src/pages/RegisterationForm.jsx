a75985c265edc9fb7843b981f6d22a6ef5fec887
Duru C. Mikael <durumikaelc@gmail.com>
2022-11-24 12:02:55 +0100

    rewrite sign up form

--- src/pages/RegisterationForm.jsx
+++ src/pages/RegisterationForm.jsx
@@ -8,64 +8,63 @@ function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [acceptTerms, setAcceptTerms] = useState('');
+  const [showInput, setShowInput] = useState(false)
 
-  const [inputname, setInputName] = useState(name);
-  const [inputemail, setInputEmail] = useState(email);
-  const [inputpassword, setInputPassword] = useState(password);
-  const [inputconfirmPassword, setInputConfirmPassword] = useState(confirmPassword);
-
-  const handleChangeName = (event) => {
-    setName(event.target.value);
+  const handleChange = (e) => {
+    const {id, value } = e.target;
+    if (id === 'name') {
+      setName(value)
+    }
+    if (id === 'email') {
+      setEmail(value)
+    }
+    if (id === 'password') {
+      setPassword(value)
+    }
+    if (id === 'confirmPassword') {
+      setConfirmPassword(value)
+    }
+    if (e.target.checked) {
+      setAcceptTerms(value)
+    } else {
+      setAcceptTerms('')
+    }
   }
-
-  const handleChangeEmail = (event) => {
-    setEmail(event.target.value);
-  }
-
-  const handleChangePassword = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleChangeConfirmPassword = (event) => {
-    setConfirmPassword(event.target.value);
-  };
 
   const handleClick = () => {
-    if (name !== "" && email !== "" && password !== "" && confirmPassword !== "" && (password === confirmPassword)) {
-      setInputName(name)
-      setInputEmail(email);
-      setInputPassword(password);
-      setInputConfirmPassword(confirmPassword);
+    if (name !== "" && email !== "" && password !== "" && confirmPassword !== "" && (password === confirmPassword) && acceptTerms !== "") {
+      setShowInput(true);
       ShowAlert("Registered Successfully", "alert-success");
-    } else {
-      ShowAlert("Please Fill in correct credentials", "alert-warning")
     }
-  };
-  
+      else {
+    ShowAlert("Please Fill in complete and correct credentials", "alert-warning")
+    }
+  }
 
   const handleSubmit = (e) => e.preventDefault();
-  
+
   return (
     <section className='container my-5 wrapper'>
       <div className="card mx-auto" style={{maxWidth: '30rem'}}>
         <form action="" className='p-4' onSubmit={handleSubmit}>
           <fieldset>
-            <legend className='fs-1 ead text-center fw-bold mb-5 mt-5'>CREATE ACCOUNT</legend>
+            <legend className='fs-1 lead text-center fw-bold my-5'>CREATE ACCOUNT</legend>
             <div className="form-group">
-              <input type="text" className="form-control" id="InputName" placeholder="Enter Name" value={name} onChange={handleChangeName} />
+              <input type="text" className="form-control" id="name" placeholder="Enter Name" value={name} onChange={(e) => handleChange(e)} />
             </div>
             <div className="form-group  mt-4">
-              <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter Email" value={email} onChange={handleChangeEmail} />
+              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter Email" value={email} onChange={(e) => handleChange(e)} />
               <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group  mt-4">
-              <input type="password" className="form-control" id="InputPassword" placeholder="Password" value={password} onChange={handleChangePassword} />
+              <input type="password" className="form-control" id="password" placeholder="Password" value={password} onChange={(e) => handleChange(e)} />
             </div>
             <div className="form-group  mt-4">
-              <input type="password" className="form-control" id="InputPassword2" placeholder="Confirm Password" value={confirmPassword} onChange={handleChangeConfirmPassword} />
+              <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => handleChange(e)} />
             </div>
             <div className="form-check  mt-4">
-              <input className="form-check-input" type="checkbox" value='' />
+              <input className="form-check-input" type="checkbox" id="acceptTerms" value='I accept the Terms & Conditions' onChange={(e) => handleChange(e)} />
               <label className="form-check-label" htmlFor="flexCheckDefault">
                 I accept the <Link className="card-link">Terms & Conditions</Link>
               </label>
@@ -83,24 +82,29 @@ function Register() {
         </form>
       </div>
 
-      <div className="card mx-auto mt-5" style={{maxWidth: '30rem'}}>
+      {(showInput) &&
+        <div className="card mx-auto mt-5" style={{maxWidth: '30rem'}}>
           <div className="card-body">
             <p className="card-text">
-              Name: {inputname}
+              Name: {name}
             </p>
             <p className="card-text">
-              Email: {inputemail}
+              Email: {email}
             </p>
             <p className="card-text">
-              Password: {inputpassword}
+              Password: {password}
             </p>
             <p className="card-text">
-              Confirm Password: {inputconfirmPassword}
+              Confirm Password: {confirmPassword}
+            </p>
+            <p className="card-text">
+              Agreement: {acceptTerms}
             </p>
           </div>
         </div>
+      } 
     </section>
-  );
+  )
 }
 
 export default Register;
