fe60136935632158cef92d5bdbbb7d54ff34992f
Duru C. Mikael <durumikaelc@gmail.com>
2022-11-24 12:03:37 +0100

    rename file from hero to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Hero() {
   return (
-    <section className="container mx-auto vh-100 d-flex flex-column justify-content-center align-items-center text-center">
+    <section className="container mx-auto d-flex flex-column justify-content-center align-items-center text-center" style={{height: '88vh'}}>
       <h1 className="display-4 fw-bold text-dark">
         Business Advisory Services
       </h1>
-      <p className="text-muted">
+      <p className="text-muted py-1">
         Disaster recovery planning, business continuity management, and disaster recovery.
       </p>
-      <button type="button" className="btn btn-outline-primary">Get Quote Now</button>
+      <button type="button" className="btn btn-outline-primary mt-2">Get Quote Now</button>
     </section>
   )
 }
